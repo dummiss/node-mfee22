f6d7df49bdb2d99cc30a108b11785bdfe83b8671
dummiss <dummiss1216@gmail.com>
2021-12-26 14:23:52 +0800

    add sum.js

---
+++ basic/sum.js
@@ -0,0 +1,13 @@
+function sum(n) {
+  for (let i = 0; i <= n; i++) {
+    let result = 0;
+    result += i;
+  }
+
+  return result;
+}
+
+console.log(sum(1));
+console.log(sum(2));
+console.log(sum(5));
+console.log(sum(10));

5af23fd8b1603551859e22b3e259343a2a596887
dummiss <dummiss1216@gmail.com>
2022-01-15 12:02:12 +0800

    1/15

---
+++ crawler/crawler3.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const { readFile } = require("fs/promises");
+
+
+
+
+(async () => {
+  try {
+    // 根據變數去抓取資料
+    let queryDate = "20220115";
+   
+    let result = await readFile("stock.txt", "utf-8")
+    let stockNo = result;
+    
+    let response = await axios.get(
+      "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+      {
+        // 這裡可以放一些設定
+        // params: 放 query string 的參數
+        params: {
+          response: "json",
+          date: queryDate,
+          stockNo,
+        },
+      }
+    );
+
+    console.log(response.data);
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+
